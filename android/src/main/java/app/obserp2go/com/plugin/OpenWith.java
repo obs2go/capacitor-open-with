8595fda752da71e7c16dd8ac6f727d59408e8dca
Rossen Marudov <rossenmarudov@Rossens-MacBook-Air.local>
2023-09-16 19:51:51 +0300

    i

--- android/src/main/java/app/obserp2go/com/plugin/OpenWith.java
+++ android/src/main/java/app/obserp2go/com/plugin/OpenWith.java
@@ -3,6 +3,8 @@ package app.obserp2go.com.plugin;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import androidx.core.content.FileProvider;
+import java.io.File;
 
 public class OpenWith {
 
@@ -13,10 +15,16 @@ public class OpenWith {
     }
 
     public void openFile(String path) {
+        File file = new File(path);
+        Uri fileUri = FileProvider.getUriForFile(
+            context,
+            context.getApplicationContext().getPackageName() + ".provider",
+            file
+        );
+
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        Uri uri = Uri.parse(path);
-        intent.setDataAndType(uri, "*/*");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setDataAndType(fileUri, "*/*");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
         context.startActivity(intent);
     }
