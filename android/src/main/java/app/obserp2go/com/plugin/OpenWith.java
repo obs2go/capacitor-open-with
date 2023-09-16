d9e2a2b69d371918823b230cd7e3c54592f3a9f3
Rossen Marudov <rossenmarudov@Rossens-MacBook-Air.local>
2023-09-16 18:36:10 +0300

    i

--- android/src/main/java/app/obserp2go/com/plugin/OpenWith.java
+++ android/src/main/java/app/obserp2go/com/plugin/OpenWith.java
@@ -8,4 +8,31 @@ public class OpenWith {
         Log.i("Echo", value);
         return value;
     }
+
+    @PluginMethod
+    public void openWith(PluginCall call) {
+        String path = call.getString("path");
+        if (path == null) {
+            call.reject("Path is required");
+            return;
+        }
+
+        File file = new File(path);
+        if (!file.exists()) {
+            call.reject("File does not exist");
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = FileProvider.getUriForFile(getContext(), getContext().getPackageName() + ".fileprovider", file);
+        intent.setDataAndType(uri, "application/*");
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        try {
+            getContext().startActivity(intent);
+            call.success();
+        } catch (ActivityNotFoundException e) {
+            call.reject("No application found to open the file");
+        }
+    }
 }
