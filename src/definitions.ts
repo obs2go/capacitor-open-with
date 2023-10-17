2de0a33e2c8494136dd86bdc59a3f93a4d7213c2
Rossen Marudov <rossenmarudov@Rossens-MacBook-Air.local>
2023-10-17 15:19:33 +0300

    making plugin importable

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,9 @@
+declare module '@capacitor/core' {
+  interface PluginRegistry {
+    Obs2goOpenWith: Obs2goOpenWithPlugin;
+  }
+}
+
 export interface OpenWithPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
 }
